20340006625b3f42286d0a6aadac9eb7c0fd24d6
Tamas Gal <himself@tamasgal.com>
2014-07-27 17:58:57 +0200

    Initial commit

---
+++ 030_aliases.sh
@@ -0,0 +1,86 @@
+alias ls='ls -G'
+alias sl="ls"
+alias vault='cd /home/vault/capn/mpo1217'
+
+alias coverage_package='coverage run --source=./ -m unittest discover'
+alias coverage_report='coverage report -m'
+
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+alias .4='cd ../../../../'
+alias .5='cd ../../../../..'
+
+alias cgrep="grep --color=auto"
+alias cegrep="egrep --color=auto"
+alias cfgrep="fgrep --color=auto"
+
+alias bc='bc -l'
+
+alias mkdir='mkdir -pv'
+
+alias path='echo -e ${PATH//:/\\n}'
+
+alias now='date +"%T"'
+alias nowtime=now
+alias nowdate='date +"%d-%m-%Y"'
+
+alias clean_pyc='rm -rf *.pyc'
+
+alias nb='ipython notebook --pylab=inline'
+
+alias vi='vim --cmd "set runtimepath+=$HOME/sandbox/rc/vim"'
+alias vless='/usr/share/vim/vim72/macros/less.sh'
+
+# safety first
+alias ln='ln -i'
+alias mv='mv -i'
+
+# system
+## pass options to free ## 
+alias meminfo='free -m -l -t'
+## get top process eating memory
+alias psmem='ps auxf | sort -nr -k 4'
+alias psmem10='ps auxf | sort -nr -k 4 | head -10'
+## get top process eating cpu ##
+alias pscpu='ps auxf | sort -nr -k 3'
+alias pscpu10='ps auxf | sort -nr -k 3 | head -10'
+## Get server cpu info ##
+alias cpuinfo='lscpu'
+## older system use /proc/cpuinfo ##
+##alias cpuinfo='less /proc/cpuinfo' ##
+## get GPU ram on desktop / laptop## 
+alias gpumeminfo='grep -i --color memory /var/log/Xorg.0.log'
+
+alias wget='wget -c'
+
+alias man="man -a"
+
+function fawk {
+    first="awk '{print "
+    last="}'"
+    cmd="${first}\$${1}${last}"
+    eval $cmd
+}
+
+alias doctest='python -m doctest'
+
+alias less='less -FSRX'
+
+alias stefannerven='write mpp460'
+
+alias qlogin_1G_1h_1G='qlogin -l fsize=1G,ct=1:00:00,vmem=1G'
+alias qlogin_2G_1h_1G='qlogin -l fsize=1G,ct=1:00:00,vmem=1G'
+alias qlogin_5G_1h_1G='qlogin -l fsize=1G,ct=1:00:00,vmem=1G'
+alias qlogin_10G_3h_1G='qlogin -l fsize=1G,ct=1:00:00,vmem=1G'
+alias qlogin_15G_5h_1G='qlogin -l fsize=1G,ct=1:00:00,vmem=1G'
+alias qlogin_20G_5h_1G='qlogin -l fsize=1G,ct=1:00:00,vmem=1G'
+
+alias notebook='ipython notebook --pylab=inline ~/Dropbox/Notebooks'
+
+alias sqlantares='sqlplus ant_read/kyoread@"(DESCRIPTION = (ADDRESS = (PROTOCOL = TCP)(HOST = ccdbora01.in2p3.fr)(PORT = 1521)) (CONNECT_DATA = (SERVER = DEDICATED) (SERVICE_NAME = antares.in2p3.fr)))"'
+
+# seatray/icetray
+alias rebuild_debug='rm -f CMakeCache.txt && cmake ../src -DSYSTEM_PACKAGES=True -DCMAKE_BUILD_TYPE=Debug && make -j3'
+alias rebuild_release='rm -f CMakeCache.txt && cmake ../src -DSYSTEM_PACKAGES=True -DCMAKE_BUILD_TYPE=Release && make -j3'
---
+++ 099_prompt.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+#############################################################################
+#
+# FILE:         099_prompt.sh
+#
+# DESCRIPTION:  my prompt
+#
+############################################################################
+
+# Configure colors, if available.
+if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
+  c_reset='\[\e[0m\]'
+  c_user='\[\e[1;36m\]'
+  c_path='\[\e[0m\]'
+  c_hostname='\[\e[0;36m\]'
+  c_time='\[\e[47m\]\[\e[30m\]'
+  c_flags='\[\e[97m\]\[\e[46m\]'
+  c_seatray='\[\e[97m\]\[\e[41m\]'
+  c_python='\[\e[97m\]\[\e[42m\]'
+  c_vimode='\[\e[97m\]\[\e[40m\]'
+  c_git='\[\e[43m\]\[\e[38;5;229m\]'
+else
+  c_reset=
+  c_user=
+  c_path=
+  c_hostname=
+  c_time=
+  c_flags=
+fi
+
+__prompt_flags ()
+{
+  #__vi_mode
+  __seatray_env
+  __git_env
+  __python_env
+}
+
+__git_env ()
+{
+  echo -n "${c_git}`__git_ps1`${c_reset}"
+}
+
+__seatray_env ()
+{
+  if [ ! -z "$I3_SHELL" ]; then
+    echo -n "${c_seatray}S${c_reset}"
+  else
+    echo -n ""
+  fi
+}
+
+__python_env ()
+{
+  if [ ! -z "$VIRTUAL_ENV" ]; then
+    echo "${c_python}`basename $VIRTUAL_ENV`${c_reset}"
+  else
+    echo -n ""
+  fi
+}
+
+__vi_mode ()
+{
+  VIMODE=`set -o | grep ^vi | awk '{print $2}'`
+  if [ "x${VIMODE}" == "xon" ]; then
+    echo -n "${c_vimode}V${c_reset}"
+  else
+    echo -n ""
+  fi
+}
+
+__current_time ()
+{
+  date +'%H:%M:%S'
+}
+
+PROMPT_COMMAND='PS1="${c_reset}`__prompt_flags`${c_flags} ${c_reset}${c_user}\u${c_reset}@${c_hostname}\h${c_reset}:${c_path}$PWD${c_reset}\n${c_time}`__current_time`${c_reset} "'
+
